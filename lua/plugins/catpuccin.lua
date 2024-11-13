2abe1c79a66460815ac027bbeb0965e791d3e945
KKomail <komailkavani7@gmail.com>
2024-11-13 16:37:55 +0800

    fixed errors and updated colour scheme to custom

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,31 +1,51 @@
 -- catppuccin_setup.lua
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  spec = {
-    -- Install Catppuccin color scheme
-    { "catppuccin/nvim", name = "catppuccin" },
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        require("catppuccin").load()
+      end,
+    },
   },
-  defaults = {
-    lazy = false, -- Load all plugins at startup
-    version = false, -- Use the latest version
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      no_italic = true,
+      term_colors = true,
+      transparent_background = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
+    },
   },
-  install = { colorscheme = { "catppuccin" } },
-})
-
--- Set Catppuccin as the default color scheme
-vim.cmd("colorscheme catppuccin")
+}
